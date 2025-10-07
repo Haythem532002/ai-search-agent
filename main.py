f1fb2bfea0dc638b5347a723333399c428f118ad
Haythem532002 <khiarih06@gmail.com>
2025-10-07 22:59:23 +0100

    add main agent code

---
+++ main.py
@@ -0,0 +1,60 @@
+from dotenv import load_dotenv
+from langchain.agents import create_tool_calling_agent, \
+    AgentExecutor
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+
+
+load_dotenv()
+
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+    
+
+llm = ChatOpenAI(model="gpt-4o")
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use necessary tools.
+            Wrap the output in the format and provide no other text\n{format_instructions}
+            """
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad")
+    ]
+).partial(format_instruction=parser.get_format_instructions())
+
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=[]
+)
+
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=[],
+    verbose=True
+)
+
+raw_response = agent_executor.invoke({"query": "What is the capital of tunisia"})
+
+print(raw_response)
+
+
+try:
+    parsed_response = parser.parse(raw_response.get("output")[0]["text"])
+except Exception as e:
+    print("Error parsing response", e, "Raw Response : ", raw_response)
+
